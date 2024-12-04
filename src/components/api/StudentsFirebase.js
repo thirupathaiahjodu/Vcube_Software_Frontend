6bf003bdf34a4fa388eb6033bf87b8b43a0db757
joduthirupathaiah <thirupathaiahjodu0421@gmail.com>
2024-12-04 11:28:42 +0530

    first commit

--- src/components/api/StudentsFirebase.js
+++ src/components/api/StudentsFirebase.js
@@ -8,13 +8,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQDiQQgZTpgK_yYPt3bO7fQZa6HNmVFH8",
-  authDomain: "my-api-xei9yl.firebaseapp.com",
-  projectId: "my-api-xei9yl",
-  storageBucket: "my-api-xei9yl.appspot.com",
-  messagingSenderId: "58137323797",
-  appId: "1:58137323797:web:177e700f9365e005db611c"
+  apiKey: "AIzaSyBnZECYeOQlNp-ykHmEpnhKZngO3dHSQf0",
+  authDomain: "thiruproject-96f99.firebaseapp.com",
+  projectId: "thiruproject-96f99",
+  storageBucket: "thiruproject-96f99.firebasestorage.app",
+  messagingSenderId: "96481708290",
+  appId: "1:96481708290:web:31e50238ee566260329a1b",
+  measurementId: "G-4YJTTCFC0K"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
